27e1656ce14d9d9a5db972460fdb73e54ae70ecf
Renuka-Bandaru <renuka.bandaru777@gmail.com>
2025-01-27 15:13:29 +0530

    react installed

---
+++ day9/script.js
@@ -0,0 +1,14 @@
+const movie1 = {
+    title: "Inception",
+    price: 200,
+    time: "10:00AM",
+    seats: 20
+};
+
+const movie2 = {
+    title: "Kalki",
+    price: 250,
+    time: "2:00PM",
+    seats: 50
+};
+
